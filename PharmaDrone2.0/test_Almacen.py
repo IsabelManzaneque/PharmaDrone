5a32d1ade69a0a11afd2edbd8c42e811cfe33409
IsabelM <Isabelvmn93@gmail.com>
2024-08-10 17:50:21 +0100

    empezando tests

---
+++ PharmaDrone2.0/test_Almacen.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 16:13:57 2024
+
+@author: Isabe
+"""
+
+import unittest
+import coverage
+import datetime
+from Almacen import Almacen
+from Paciente import Paciente
+from Pedido import Pedido
+from Dron import Dron
+from unittest.mock import patch
+
+class TestAlmacen(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        # util para hacer algo una sola vez antes de correr todos los test
+        # a diferencia de setUp que se ejecuta antes de cada test. Ej: rellenar
+        # una BBDD para ser utilizada en los test
+        pass
+        
+    @classmethod
+    def tearDownClass(cls):
+        # util para hacer algo una sola vez despues de correr todos los test
+        # a diferencia de tearDown que se ejecuta despues de cada test. Ej: 
+        # eliminar la BBDD tras ser utilizada en los test
+        pass
+    
+    def setUp(self):
+        self.newAlmacen = Almacen(1, "newDir", "newMun", "newPro", "newDesc")    
+        
+    def test_init(self):
+        testAmlacen = Almacen(2, "testDir", "testMun", "testPro", "testDesc")
+        self.assertEquals(testAmlacen.idAlmacen, 2)
+        self.assertEquals(testAmlacen.direccion, "testDir")
+        self.assertEquals(testAmlacen.municipio, "testMun")
+        self.assertEquals(testAmlacen.provincia, "testPro")
+        self.assertEquals(testAmlacen.descripcion, "testDesc")
+        self.assertIsInstance(testAmlacen.pacientes, dict)
+        self.assertIsInstance(testAmlacen.pedidos, dict)
+        self.assertIsInstance(testAmlacen.dron, Dron)
+   
+    def test_addPaciente(self):
+        newPatientId = 1
+        newPaciente = Paciente(1, "name", 2, 2)        
+        self.newAlmacen.addPaciente(1, newPaciente)
+        pacientes = self.newAlmacen.getPacientes()        
+        self.assertEqual(pacientes[newPatientId], newPaciente)
+        
+    def test_addPedido(self):
+        newPedidoId = 1
+        newPedido = Pedido(1,1,1,datetime.date(2025, 3, 1))       
+        self.newAlmacen.addPedido(newPedidoId, newPedido)
+        pedidos = self.newAlmacen.getPedidos()
+        self.assertEqual(pedidos[newPedidoId], newPedido)
+        
+    def test_getId(self):        
+        self.assertIsInstance(self.newAlmacen.getId(), int)
+        
+    def test_getPacientes(self):        
+        self.assertIsInstance(self.newAlmacen.getPacientes(), dict)
+        
+    def test_getPedidos(self):        
+        self.assertIsInstance(self.newAlmacen.getPedidos(), dict)
+        
+    def test_getDron(self):        
+        self.assertIsInstance(self.newAlmacen.getDron(), Dron)
+        
+    @patch('builtins.print')
+    def test_mostrarInfo(self, mock_print):
+        self.newAlmacen.mostrarInfo() 
+        mock_print.assert_called_with('\nAlmacen 1:\n - Calle: newDir\n - Municipio: newMun\n - Provincia: newPro\n - Descripccion: newDesc')
+        
+        
+if __name__ == '__main__':
+    
+    # Iniciar la cobertura
+    cov = coverage.Coverage()
+    cov.start()
+    
+    unittest.main(exit=False)
+    
+    # Detener, guardar la cobertura y mostrar reporte
+    cov.stop()
+    cov.save()
+    cov.report()
+
+
+        
+        
---
+++ PharmaDrone2.0/test_main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 16:09:23 2024
+
+@author: Isabe
+"""
+
+import unittest
+import main
+
+
+class TestMain(unittest.TestCase):
+    pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
